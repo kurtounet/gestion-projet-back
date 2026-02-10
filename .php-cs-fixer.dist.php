83167e0cb9b2230df9b648e4c382d2b61970a355
kurtounet <bossut.anthony@gmail.com>
2026-02-10 19:00:18 +0100

    add rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,9 +1,25 @@
 <?php
-
+$config = new PhpCsFixer\Config();
+return $config
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect()) // Ajout de cette ligne
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'not_operator_with_successor_space' => true,
+        'trailing_comma_in_multiline' => true,
+        'phpdoc_scalar' => true,
+        'unary_operator_spaces' => true,
+        'binary_operator_spaces' => true,
+        'blank_line_before_statement' => [
+            'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
+        ],
+    ])
+    ->setFinder($finder);
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->exclude('var')
-;
+    ->exclude('var');
 
 return (new PhpCsFixer\Config())
     ->setRules([
